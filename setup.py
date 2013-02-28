6e4ceb1af94061938d8fd382ee8139f261b5b0f0
Rafael Feijó da Rosa <rafael@desenvolvendoweb.com.br>
2013-02-28 09:38:09 -0300

    criando comandos 2

--- setup.py
+++ setup.py
@@ -4,6 +4,11 @@
 import os
 import sys
 
+from subprocess import Popen, PIPE
+
+MODPATH = os.path.abspath(os.path.dirname(__file__))
+abspath = lambda path: os.path.join(MODPATH, path)
+
 try:
     project  = sys.argv[1]
 except:
@@ -18,15 +23,38 @@ try:
     install  = sys.argv[3]
 except:
     install  = ''
+    
+
+class Cmd(object):
+    def __init__(self, cmd):
+        self.cmd = cmd
+    def __call__(self, *args):
+        command = '%s %s' % (self.cmd, ' '.join(args))
+        result = Popen(command, stdout=PIPE, stderr=PIPE, shell=True)
+        return result.communicate()[0]
+
+
+class Sh(object):
+    def __getattr__(self, attribute):
+        return Cmd(attribute)
+
 
 if install and install == '--install':
     print 'preparando para instalar'
     
+    sh = Sh()
+    
     list_program = ['mysql-client', 'mysql-server', 'nginx']
 
-    os.system('sudo apt-get install %s' % ' '.join(list_program))
+    sh.sudo('apt-get install %s' % ' '.join(list_program))
     
-    sys.exit('stop')
+    if not os.path.isdir('/var/www/'):
+        #sh.mkdir('/var/www/')
+        #sh.cp('')
+        print MODPATH
+        print abspath
+    
+    sys.exit('Terminou com sucesso! Abra o navegador e digite http://localhost ou IP da maquina.')
 
 
 os.system('mysql -u root -p%s %s < /var/www/tcd.sql' % (password, project))
