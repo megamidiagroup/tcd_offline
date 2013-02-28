f325504a61afdc78885968201e0ce8a48ef0b013
Rafael Feijó da Rosa <rafael@desenvolvendoweb.com.br>
2013-02-28 17:33:14 -0300

    criando comandos 36

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ if install and install == '--install':
         sp = subprocess.Popen(["/bin/bash", "-i", "-c", "mkvirtualenv tcd"])
         sp.communicate()
         
-        sp = subprocess.Popen(["/bin/bash", "-i", "-c", "source $WORKON_HOME/bin/activate"])
+        sp = subprocess.Popen(["/bin/bash", "-i", "-c", "source $WORKON_HOME/tcd/bin/activate"])
         sp.communicate()
 
         list_program = ['yolk', 'Django==1.3.2']
