943a8b1bace968adf21694a1a06754884abf6d9f
Rafael Feijó da Rosa <rafael@desenvolvendoweb.com.br>
2013-03-01 09:21:14 -0300

    criando comandos 37

---
+++ pip.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export WORKON_HOME=/var/www/tcd_offline/.virtualenvs
+
+source /usr/local/bin/virtualenvwrapper.sh
+
+mkvirtualenv tcd
+
+pip install yolk Django==1.3.2
--- setup.py
+++ setup.py
@@ -69,22 +69,7 @@ if install and install == '--install':
         if not os.path.exists('/usr/local/bin/virtualenvwrapper.sh'):
             os.system('cd /tmp/; git clone https://github.com/bernardofire/virtualenvwrapper.git; cd virtualenvwrapper; python setup.py install')
             
-        sp = subprocess.Popen(["/bin/bash", "-i", "-c", "export WORKON_HOME=/var/www/tcd_offline/.virtualenvs"])
-        sp.communicate()
-        
-        sp = subprocess.Popen(["/bin/bash", "-i", "-c", "source /usr/local/bin/virtualenvwrapper.sh"])
-        sp.communicate()
-        
-        sp = subprocess.Popen(["/bin/bash", "-i", "-c", "mkvirtualenv tcd"])
-        sp.communicate()
-        
-        sp = subprocess.Popen(["/bin/bash", "-i", "-c", "source $WORKON_HOME/tcd/bin/activate"])
-        sp.communicate()
-
-        list_program = ['yolk', 'Django==1.3.2']
-        
-        sp = subprocess.Popen(["/bin/bash", "-i", "-c", "pip install %s" % ' '.join(list_program)])
-        sp.communicate()
+        os.system('./pip.sh')
         
     sh.find('/etc/nginx/nginx.conf -type f -exec sed -i "s/www-data/root/g" {} \;')
     
