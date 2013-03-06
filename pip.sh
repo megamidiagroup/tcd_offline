fc6f19c2b8f3544cb705332a1f34b7b4da7c3a1e
Rafael Feijó da Rosa <rafael@desenvolvendoweb.com.br>
2013-03-06 10:05:16 -0300

    alterados os itens de sync 6

--- pip.sh
+++ pip.sh
@@ -71,6 +71,13 @@ fi
 cd yolk
 python setup.py install
 
+echo ">>>>>>>>>>>>>>>>>>>>>>"
+
+python -c 'import yolk'
+echo "yolk >> ok"
+
+echo "<<<<<<<<<<<<<<<<<<<<<<"
+
 ### pip install yolk
 
 cd /tmp
@@ -96,6 +103,13 @@ EOD
 
 cd django
 python setup.py install
+
+echo ">>>>>>>>>>>>>>>>>>>>>>"
+
+python -c 'import django'
+echo "django >> ok"
+
+echo "<<<<<<<<<<<<<<<<<<<<<<"
 
 ### pip install Django==1.3.2
 
@@ -123,6 +137,13 @@ EOD
 cd django-simple-captcha
 python setup.py install
 
+echo ">>>>>>>>>>>>>>>>>>>>>>"
+
+python -c 'import captcha'
+echo "captcha >> ok"
+
+echo "<<<<<<<<<<<<<<<<<<<<<<"
+
 ### pip install django-simple-captcha
 
 cd /tmp
@@ -148,6 +169,13 @@ EOD
 
 cd django-mobile
 python setup.py install
+
+echo ">>>>>>>>>>>>>>>>>>>>>>"
+
+python -c 'import django_mobile'
+echo "django_mobile >> ok"
+
+echo "<<<<<<<<<<<<<<<<<<<<<<"
 
 ### pip install django_mobile
 
@@ -175,6 +203,13 @@ EOD
 cd south
 python setup.py install
 
+echo ">>>>>>>>>>>>>>>>>>>>>>"
+
+python -c 'import south'
+echo "south >> ok"
+
+echo "<<<<<<<<<<<<<<<<<<<<<<"
+
 ### pip install south
 
 cd /tmp
@@ -200,6 +235,12 @@ EOD
 
 cd mysql-python
 python setup.py install
+
+echo ">>>>>>>>>>>>>>>>>>>>>>"
+
+echo "mysql-python >> ok"
+
+echo "<<<<<<<<<<<<<<<<<<<<<<"
 
 ### pip install MySQL-python
 
@@ -227,6 +268,12 @@ EOD
 cd django-ckeditor
 python setup.py install
 
+echo ">>>>>>>>>>>>>>>>>>>>>>"
+
+echo "django-ckeditor >> ok"
+
+echo "<<<<<<<<<<<<<<<<<<<<<<"
+
 ### pip install django-ckeditor
 
 cd /tmp
@@ -252,6 +299,13 @@ EOD
 
 cd django-crequest
 python setup.py install
+
+echo ">>>>>>>>>>>>>>>>>>>>>>"
+
+python -c 'import crequest'
+echo "crequest >> ok"
+
+echo "<<<<<<<<<<<<<<<<<<<<<<"
 
 ### pip install django-crequest
 
@@ -279,6 +333,13 @@ EOD
 cd BeautifulSoup
 python setup.py install
 
+echo ">>>>>>>>>>>>>>>>>>>>>>"
+
+python -c 'import BeautifulSoup'
+echo "BeautifulSoup >> ok"
+
+echo "<<<<<<<<<<<<<<<<<<<<<<"
+
 ### pip install BeautifulSoup
 
 cd /tmp
@@ -304,6 +365,13 @@ EOD
 
 cd flup
 python setup.py install
+
+echo ">>>>>>>>>>>>>>>>>>>>>>"
+
+python -c 'import flup'
+echo "flup >> ok"
+
+echo "<<<<<<<<<<<<<<<<<<<<<<"
 
 ### pip install flup
 
@@ -331,6 +399,13 @@ EOD
 cd django-flash
 python setup.py install
 
+echo ">>>>>>>>>>>>>>>>>>>>>>"
+
+python -c 'import djangoflash'
+echo "djangoflash >> ok"
+
+echo "<<<<<<<<<<<<<<<<<<<<<<"
+
 ### pip install django-flash
 
 cd /tmp
@@ -356,6 +431,13 @@ EOD
 
 cd django-tagging
 python setup.py install
+
+echo ">>>>>>>>>>>>>>>>>>>>>>"
+
+python -c 'import tagging'
+echo "tagging >> ok"
+
+echo "<<<<<<<<<<<<<<<<<<<<<<"
 
 ### pip install django-tagging
 
@@ -383,6 +465,13 @@ EOD
 cd PIL
 python setup.py install
 
+echo ">>>>>>>>>>>>>>>>>>>>>>"
+
+python -c 'import PIL; import Image'
+echo "PIL >> ok"
+
+echo "<<<<<<<<<<<<<<<<<<<<<<"
+
 ### pip install PIL
 
 cd /tmp
@@ -408,6 +497,13 @@ EOD
 
 cd ReportLab
 python setup.py install
+
+echo ">>>>>>>>>>>>>>>>>>>>>>"
+
+python -c 'import reportlab'
+echo "reportlab >> ok"
+
+echo "<<<<<<<<<<<<<<<<<<<<<<"
 
 ### pip install reportlab
 
@@ -435,6 +531,13 @@ EOD
 cd simplejson
 python setup.py install
 
+echo ">>>>>>>>>>>>>>>>>>>>>>"
+
+python -c 'import simplejson'
+echo "simplejson >> ok"
+
+echo "<<<<<<<<<<<<<<<<<<<<<<"
+
 ### pip install simplejson
 
 cd /tmp
@@ -460,6 +563,13 @@ EOD
 
 cd cython
 python setup.py install
+
+echo ">>>>>>>>>>>>>>>>>>>>>>"
+
+python -c 'import cython'
+echo "cython >> ok"
+
+echo "<<<<<<<<<<<<<<<<<<<<<<"
 
 ### pip install "Cython>=0.17.3"
 
@@ -487,6 +597,13 @@ EOD
 cd lxml
 python setup.py install
 
+echo ">>>>>>>>>>>>>>>>>>>>>>"
+
+python -c 'import lxml'
+echo "lxml >> ok"
+
+echo "<<<<<<<<<<<<<<<<<<<<<<"
+
 ### pip install lxml
 
 cd /tmp
@@ -512,6 +629,13 @@ EOD
 
 cd PyAMF
 python setup.py install
+
+echo ">>>>>>>>>>>>>>>>>>>>>>"
+
+python -c 'import pyamf'
+echo "pyamf >> ok"
+
+echo "<<<<<<<<<<<<<<<<<<<<<<"
 
 ### pip install pyamf
 
@@ -539,6 +663,13 @@ EOD
 cd django-grappelli
 python setup.py install
 
+echo ">>>>>>>>>>>>>>>>>>>>>>"
+
+python -c 'import grappelli'
+echo "grappelli >> ok"
+
+echo "<<<<<<<<<<<<<<<<<<<<<<"
+
 ### pip install django-grappelli
 
 cd /tmp
@@ -565,6 +696,13 @@ EOD
 cd django-colors
 python setup.py install
 
+echo ">>>>>>>>>>>>>>>>>>>>>>"
+
+python -c 'import colors'
+echo "colors >> ok"
+
+echo "<<<<<<<<<<<<<<<<<<<<<<"
+
 cd /tmp
 /usr/bin/expect <<EOD
     spawn /bin/sh -c "git clone ssh://deploy@$URL/var/django/tcd_offline/dependences/django-admin-tools.git"
@@ -588,4 +726,11 @@ EOD
 
 cd django-admin-tools
 python setup.py install
+
+echo ">>>>>>>>>>>>>>>>>>>>>>"
+
+python -c 'import admin_tools'
+echo "admin_tools >> ok"
+
+echo "<<<<<<<<<<<<<<<<<<<<<<"
 
