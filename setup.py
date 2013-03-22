e86433dfcdba23d14dac712e24ec911cbffdbe58
Rafael Feijó da Rosa <rafael@desenvolvendoweb.com.br>
2013-03-22 16:10:16 -0300

    arrumado setup e demais itens 2

--- setup.py
+++ setup.py
@@ -117,10 +117,6 @@ if action.count('--install') == 1 or action.count('--upgrade') == 1:
     sh.find('/var/www/tcd_offline/sync.sh -type f -exec sed -i "s/<rede>/%s/g" {} \;' % rede)
     sh.find('/var/www/tcd_offline/ftp.sh -type f -exec sed -i "s/<rede>/%s/g" {} \;' % rede)
 
-    if len(sh.grep('-ir "/etc/init.d/tcd" /etc/rc.local')) == 0:
-        sh.find('/etc/rc.local -type f -exec sed -i "s/exit\ 0//g" {} \;')
-        os.system('echo "(exec /etc/init.d/tcd)" >> /etc/rc.local; echo "exit 0" >> /etc/rc.local')
-    
     if len(sh.grep('-ir "/var/www/tcd_offline/sync.sh" /etc/crontab')) == 0:
         os.system('echo "00 00 * * * root (cd / && /var/www/tcd_offline/sync.sh >> /var/log/tcd/sync.log 2>&1)" >> /etc/crontab')
         
