b0d055d2bfdb0ab317c16e14750a5c2ec77cce41
Rafael Feijó da Rosa <rafael@desenvolvendoweb.com.br>
2013-04-15 12:38:15 -0300

    aplicado bloqueios

--- kiosk.sh
+++ kiosk.sh
@@ -9,5 +9,6 @@ xmodmap -e 'keycode 37 = 0x0000' #disables Ctrl_L
 xmodmap -e 'keycode 105 = 0x0000' #disables Ctrl_R
 xmodmap -e 'keycode 64 = 0x0000' #disables Alt_L
 xmodmap -e 'keycode 108 = 0x0000' #disables Ctrl_R
+xmodmap -e 'keycode  67 = F1 XF86_Switch_VT_2' #hability F1 para Ctrl + Alt + F2
 xsetroot -cursor_name left_ptr
 while true; do /usr/bin/firefox http://localhost/loading.html -width $width -height $height; sleep 5s; done
